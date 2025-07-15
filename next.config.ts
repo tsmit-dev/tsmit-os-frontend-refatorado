a509d30176819242dcfd2e1c9cf4e3d46597f7e5
TSMIT - Desenvolvimento <dev@tsmit.com.br>
2025-07-15 16:11:23 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Ignora erros de lint durante o build
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
